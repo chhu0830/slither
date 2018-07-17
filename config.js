069ce9972b01cbb9e96907bed7ee6847ccde10dc
oHSUANo <chhu0830@gmail.com>
2018-07-18 05:41:37 +0800

    Add config file

---
+++ config.js
@@ -0,0 +1,19 @@
+var snake1_config = { "speed": 5,
+                      "direction": 0,
+                      "radius": 32,
+                      "size": 10,
+                      "color": "red",
+                      "body": ["#ff6566", "#ff6966", "#ff8266", "#ff9267", "#ff9f63", "#ff9d64", "#ffb764", "#fcb865", "#ffbf68", "#fcb865", "#ffb764", "#ff9d64", "#ff9f63", "#ff9267", "#ff9267", "#ff8266", "#ff6966", "#ff6966"]
+                    }
+
+
+var snake2_config = { "speed": 5,
+                      "direction": -Math.PI,
+                      "radius": 32,
+                      "size": 10,
+                      "color": "blue",
+                      "body": ["#FF0000", "#FFA500", "#FFFF00", "#00FF00", "#007FFF", "#0000FF", "#8B00FF"]
+                    }
+
+var skillList = [Slow, PushFood];
+var playTime = 60;
