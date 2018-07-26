e9c1963ad7da7357ff39b950b0d82dfd0e38a464
oHSUANo <chhu0830@gmail.com>
2018-07-26 20:54:29 +0800

    Add skills of participants

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
+// Default skins
 var def = {"head": "http://icons.iconarchive.com/icons/seanau/flat-smiley/256/Smiley-11-icon.png",
            "body": ["#ff6566", "#ff6966", "#ff8266", "#ff9267",
                     "#ff9f63", "#ff9d64", "#ffb764", "#fcb865",
                     "#ffbf68", "#fcb865", "#ffb764", "#ff9d64",
                     "#ff9f63", "#ff9267", "#ff9267", "#ff8266",
                     "#ff6966", "#ff6966"] };
-var snake1 = { "head": "./img/team1.png",
+var def2 = {"head": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT28AkmHMtY761MOEaGcJcPwGK1dvAOuevG_tSVuCRFWehq2uDj",
+           "body": ["#ff6566", "#ff6966", "#ff8266", "#ff9267",
+                    "#ff9f63", "#ff9d64", "#ffb764", "#fcb865",
+                    "#ffbf68", "#fcb865", "#ffb764", "#ff9d64",
+                    "#ff9f63", "#ff9267", "#ff9267", "#ff8266",
+                    "#ff6966", "#ff6966"] };
+
+var snake1 = { "head": "https://i.imgur.com/7k2Alxk.png",
                "body": ["#467500", "#548c00", "#64a600", "#73bf00",
                         "#82d900", "#8cea00", "#9aff02", "#a8ff24",
                         "#b7ff4a", "#c2ff68", "#ccff80", "#d3ff93",
@@ -16,7 +24,7 @@ var snake3 = { "head": "https://i.imgur.com/vB0kDwd.png",
                         "#ffbf68", "#fcb865", "#ffb764", "#ff9d64",
                         "#ff9f63", "#ff9267", "#ff9267", "#ff8266",
                         "#ff6966", "#ff6966"] };
-var snake4 = { "head": "./img/team4.png",
+var snake4 = { "head": "https://i.imgur.com/RH12aM7.png",
                "body": ["#ff6566", "#ff6966", "#ff8266", "#ff9267",
                         "#ff9f63", "#ff9d64", "#ffb764", "#fcb865",
                         "#ffbf68", "#fcb865", "#ffb764", "#ff9d64",
@@ -30,7 +38,7 @@ var snake8 = def;
 var snake9 = { "head": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-TK-XeSnHhjMPKsk4hKr22MKPOBLg7I338Kh-NAQdcj8Ouq0M",
                "body": ["#808080", "#C0C0C0", "#F5F5F5", "#FFFAFA",
                         "#ff9f63", "#E6C3C3", "#BC8F8F" ] };
-var snake0 = { "head": "./img/team10.png",
+var snake0 = { "head": "https://i.imgur.com/a0aB5Vr.jpg",
                "body": ["#ff6566", "#ff6966", "#ff8266", "#ff9267",
                         "#ff9f63", "#ff9d64", "#ffb764", "#fcb865",
                         "#ffbf68", "#fcb865", "#ffb764", "#ff9d64",
@@ -41,18 +49,17 @@ var snake1_config = { "speed": 5,
                       "direction": 0,
                       "radius": 32,
                       "size": 15,
-                      "head": snake2["head"],
-                      "body": snake2["body"] }
+                      "head": def["head"],
+                      "body": def["body"] }
 
 var snake2_config = { "speed": 5,
                       "direction": -Math.PI,
                       "radius": 32,
                       "size": 15,
-                      "head": snake0["head"],
-                      "body": snake0["body"] }
+                      "head": def2["head"],
+                      "body": def2["body"] }
 
 var skillList = [ 
-                  Slow, Transport, Night,
                   Explosion, Trans,
                   Cut,
                   TimeBack, Bigger, Faster,
